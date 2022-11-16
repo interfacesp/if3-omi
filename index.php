e2191221e28d04eca813b65a0de4e2bb9bb955db
Espoir Gahungere <espoir.gahungere@interface3.be>
2022-11-16 12:23:43 +0100

    if3-omi

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Interface3 - Home</title>
+</head>
+<body>
+        <header>
+
+            <div class="style-logo">
+                <a href="index.php">   
+                    <img src="medias/images/logo-if3.png" alt="Logo Interface3">
+                </a>
+            </div>
+
+        <?php
+           include("nav.php");
+        ?>
+        </header>
+    
+        
+        <?php
+
+            if(isset($_GET["page"])){
+                $maPage= $_GET["page"];
+
+                switch ($maPage) {
+                    case 'formation':
+                        include("formations.html");
+                        break;
+
+                    case 'recrutement':
+                        include("recrutement.html");
+                        break;
+
+                    case 'sensibiliser':
+                        include("sensibiliser.html");
+                        break;
+                    
+                    case 'contact':
+                        include("contact.html");
+                        break;
+
+                    default:
+                        include("home.php");
+                        break;
+                }   
+
+
+            }else {
+                include("home.php");
+            }
+        ?>
+
+        
+        <footer>
+            <div class="style-footer">
+                <p>
+                    Fait avec amour par OMI 14! 
+                </p>
+                <p>
+                    L'informatique au Féminin
+                </p>
+                <p>
+                    OMI 14 - 2022
+                </p>
+            </div>
+     
+        </footer>
+</body>
+</html>
---
+++ nav.php
@@ -0,0 +1,8 @@
+<nav>
+    <ul>
+        <li><a href="index.php?page=formation">Se former</a></li>
+        <li><a href="index.php?page=recrutement">Recruter</a></li>
+        <li><a href="index.php?page=sensibiser">Sensibiliser</a></li>
+        <li><a href="index.php?page=contact">Contact</a></li>
+    </ul>      
+</nav>
